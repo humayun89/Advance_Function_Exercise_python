6bff9853ef226a990a4def7bdbfb7a7b4c22709c
HumayunRChy <humayun.chowdhury@metropolia.fi>
2022-11-20 19:54:20 +0200

    Exercise of lambda function

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 # Uses of lambda function:-
 div =lambda x,y: x/y
 print(div(3,2))
+#  Using input :
+x=int(input(" Enter a number:-"))
+y=int(input(" Enter a number :- "))
+div =lambda x,y: x/y
+print(div(x,y))
