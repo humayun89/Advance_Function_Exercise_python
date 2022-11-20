b76c157ac9308cd43bc3a9604b41b2d059393564
HumayunRChy <humayun.chowdhury@metropolia.fi>
2022-11-20 20:40:47 +0200

    Exercise of lambda function Using input into lambda function conventional way of division

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# Initial division system :-
+x=int(input(" Enter a number:-"))
+y=int(input(" Enter a number :- "))
+def div(x,y):
+    return (x/y)
+print(div(x,y))
+
 # Uses of lambda function:-
 div =lambda x,y: x/y
 print(div(3,2))
