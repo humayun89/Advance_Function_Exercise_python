d65002c371c2a4e0d65225305e13c904bc958653
HumayunRChy <humayun.chowdhury@metropolia.fi>
2022-11-20 21:10:48 +0200

    Exercise of lambda function Using input into lambda function conventional way of division and explanation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ y=int(input(" Enter a number :- "))
 def div(x,y):
     return (x/y)
 print(div(x,y))
+""" div is not a variable , itself is a function . As a result we call the function in print rather than a varible in it."""
 
 # Uses of lambda function:-
 div =lambda x,y: x/y
